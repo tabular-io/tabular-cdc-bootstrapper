28da3711ed309a9afb7c0d734e2a7ab4d792e60e
🌞Randy Pitcher <randypitcherii@gmail.com>
2023-12-05 17:35:55 -0500

    added params for tabular connectivity

--- tabular-cdc-bootstrapper/cdc_bootstrap_handler.py
+++ tabular-cdc-bootstrapper/cdc_bootstrap_handler.py
@@ -1,9 +1,16 @@
 import json
 import logging
+import os
+
+# ENVs
+TABULAR_CREDENTIAL       = os.environ['TABULAR_CREDENTIAL']
+TABULAR_CATALOG_URI      = os.environ['TABULAR_CATALOG_URI']
+TABULAR_TARGET_WAREHOUSE = os.environ['TABULAR_TARGET_WAREHOUSE']
 
 # Set up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
 
 def handle_new_file(event, context):
     logger.info('Function loaded successfully')
