6e7c5d0be207862bf1edbc194a2e561e6c09ad66
🌞Randy Pitcher <randypitcherii@gmail.com>
2023-12-11 09:50:07 -0500

    added important cleanup step to table creation tests

--- batch-bootstrapper/test_tabular.py
+++ batch-bootstrapper/test_tabular.py
@@ -40,26 +40,33 @@ class TestTabular:
       tabular.extract_database_and_table(s3_key, 'cdc-bootstrap/alpha')
 
   def test_bootstrap_cdc_target(self):
-    test_cases = {
+    target_db    = '_cdc_bootstrapper_ci'
+    target_table = 'missing-table'
+    test_cases   = {
       'table_exists':     ('cdc-bootstrap/system/catalog_events/my-file.json', 'cdc-bootstrap', False),
-      'table_missing':    ('cdc-bootstrap/fingers-crossed-this-doesnt-exist/missing-table/my-file.json', 'cdc-bootstrap', True),
-      'database_missing': ('cdc-bootstrap/pyiceberg/alpha/my-file.json', '', True)
+      'table_missing':    (f'cdc-bootstrap/{target_db}/{target_table}/my-file.json', 'cdc-bootstrap', True),
     }
 
-    for key in test_cases:
-      test_case = test_cases[key]
-      expected  = test_case[2]
-      actual    = tabular.bootstrap_cdc_target(
-        test_case[0], 
-        test_case[1], 
-        self.S3_BUCKET_TO_MONITOR, 
-        self.CATALOG_PROPERTIES
-      )
-      assert actual == expected
-    
-    # test some junk
-    with pytest.raises(ValueError):
-      tabular.bootstrap_cdc_target('lkdfj.jdsfskl', 'fdassdf', 'dklfjasldkf', {})
+    try:
+      for key in test_cases:
+        test_case = test_cases[key]
+        expected  = test_case[2]
+        actual    = tabular.bootstrap_cdc_target(
+          test_case[0], 
+          test_case[1], 
+          self.S3_BUCKET_TO_MONITOR, 
+          self.CATALOG_PROPERTIES
+        )
+        assert actual == expected
+      
+      # test some junk
+      with pytest.raises(ValueError):
+        tabular.bootstrap_cdc_target('lkdfj.jdsfskl', 'fdassdf', 'dklfjasldkf', {})
+
+    finally:
+      self.catalog.drop_table(f'{target_db}.{target_table}')
+      self.catalog.drop_namespace(target_db)
+
 
   def test_create_file_loader_target_table(self):
     target_db_name = '_test_cdc_bootloader'
